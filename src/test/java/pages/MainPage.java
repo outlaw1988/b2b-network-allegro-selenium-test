9897e3ea9ca0d043f5610d938d8a9f7a9767009c
Jakub Zamojski <kuba.zamojski@gmail.com>
2020-03-16 23:32:22 +0100

    Assertj removed, explicit wait added

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class MainPage {
 
@@ -28,6 +30,8 @@ public class MainPage {
     }
 
     public void enterPhraseToSearchingBoxAndConfirm(String phrase) {
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOf(searchingBoxEl));
         searchingBoxEl.sendKeys(phrase);
         searchButtonEl.click();
     }
